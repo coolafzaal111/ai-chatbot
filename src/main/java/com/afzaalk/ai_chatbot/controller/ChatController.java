a0fd79cec06f4216dbeeed85497001dd0533e208
afzaalkhan <afzaalkhan@localhost>
2026-03-30 10:41:55 +0300

    Adding AI configuratins and Chat endpoint

---
+++ src/main/java/com/afzaalk/ai_chatbot/controller/ChatController.java
@@ -0,0 +1,56 @@
+package com.afzaalk.ai_chatbot.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+	
+	@Autowired
+	private ChatModel chatModel;
+	
+	// SPring AI's fluent api for talking to AI models
+//	private final ChatClient chatClient;
+//	
+//	public ChatController (ChatModel chatModel) {
+//		// wrap ChstModel in ChatCLient using builder pattern
+//		this.chatClient = ChatClient.builder(chatModel).build();
+//	}
+	
+	@GetMapping("/test")
+    public String test() {
+        return "Spring Boot is working!";
+    }
+	
+	@PostMapping("/chat")
+	public String chat(
+			@RequestHeader(value = "Ai-Model", defaultValue = "gpt-4o-mini") String model, 
+			@RequestBody String messageInput) {
+		
+		ChatClient chatClient = ChatClient.builder(chatModel).build();
+		return chatClient
+				.prompt()			// start building the prompt
+				.user(messageInput)	// add a user message	
+				.options(OpenAiChatOptions.builder()
+						.model(model)
+						.temperature(1.0)
+						.maxCompletionTokens(200)
+						.build())
+				.call()				// execute the request
+				.content();			// extracting the text from response
+		
+	}
+	
+
+}
